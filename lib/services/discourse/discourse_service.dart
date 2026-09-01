b2d0d60dbc118f244a55ca6b5b05922f6b886c64
stevessr <2984716340@qq.com>
2026-09-01 17:58:18 +0800

    perf: wire selective switch cookie cleanup

--- lib/services/discourse/discourse_service.dart
+++ lib/services/discourse/discourse_service.dart
@@ -30,6 +30,7 @@ import '../../constants.dart';
 import '../../providers/message_bus_providers.dart';
 import '../auth_session.dart';
 import '../account_manager.dart';
+import '../account_switch_browser_cookie_cleaner.dart';
 import '../auth_issue_notice_service.dart';
 import '../cf_clearance_refresh_service.dart';
 import '../network/cookie/csrf_token_service.dart';
