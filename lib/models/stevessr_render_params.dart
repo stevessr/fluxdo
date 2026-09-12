46f896b53d62f63cb13b6c9e0a2784f9ba1c473d
stevessr <2984716340@qq.com>
2026-09-13 03:24:57 +0800

    feat(stevessr): add more expression variants

--- lib/models/stevessr_render_params.dart
+++ lib/models/stevessr_render_params.dart
@@ -1,7 +1,21 @@
 import 'dart:ui';
 
 /// StevesSR 可用的角色表情。
-enum StevessrExpression { neutral, pout, smug, surprised, happy, cry, original }
+enum StevessrExpression {
+  neutral,
+  pout,
+  smug,
+  surprised,
+  happy,
+  cry,
+  original,
+  wink,
+  angry,
+  shy,
+  thinking,
+  sleepy,
+  confused,
+}
 
 /// StevesSR 可用的气泡造型。
 enum StevessrBubble { thought, speech, cloud, shout, rounded, caption }
