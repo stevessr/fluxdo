ca3bce678738db54ef6b51ca221adef66d4586ac
stevessr <2984716340@qq.com>
2026-09-15 08:44:10 +0800

    fix(matrix): import XFile explicitly for media viewer

--- lib/pages/chat/matrix_message_media_view.dart
+++ lib/pages/chat/matrix_message_media_view.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:cross_file/cross_file.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:open_filex/open_filex.dart';
